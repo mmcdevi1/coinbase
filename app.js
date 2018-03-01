2415e19c7cb8faf3ff37719b9450a52d85bde9d0
Michael McDevitt <mmcdevi1@gmail.com>
2018-03-01 14:01:06 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+
+const routes = require('./routes');
+
+app.use('/static', express.static('public'));
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+
+// Set view engine with Pug
+app.set('view engine', 'pug');
+
+// Set up routes
+app.use(routes);
+
+// app.use(function (req, res, next) {
+//   const err = new Error('Page not found');
+//   err.status = 404;
+//   next(err);
+// })
+
+// Listen on port 3000
+app.listen(3000, function () {
+  console.log('Listening to port 3000 ..')
+})
