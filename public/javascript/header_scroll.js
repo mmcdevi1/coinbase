16fa7fd73c885f5a3d92967a37fa653e71a40245
Michael McDevitt <mmcdevi1@gmail.com>
2018-03-02 12:13:18 -0500

    update text slider on hero image

--- public/javascript/header_scroll.js
+++ public/javascript/header_scroll.js
@@ -29,7 +29,7 @@ $('a[href*=#]:not([href=#])').click(function() {
    $(".rotate").textrotator({
       animation: "dissolve", // You can pick the way it animates when rotating through words. Options are dissolve (default), fade, flip, flipUp, flipCube, flipCubeUp and spin.
       separator: ",", // If you don't want commas to be the separator, you can define a new separator (|, &, * etc.) by yourself using this field.
-      speed: 3000 // How many milliseconds until the next word show.
+      speed: 5000 // How many milliseconds until the next word show.
     });
 
 });
