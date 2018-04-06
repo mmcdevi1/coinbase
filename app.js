de2bc5cd0653f1baec74a3c519131b58dc2ddb9b
Michael McDevitt <mmcdevi1@gmail.com>
2018-04-06 14:18:15 -0400

    setup development and production secret keys

--- app.js
+++ app.js
@@ -1,23 +1,28 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+const passport = require('passport');
+const bodyParser = require('body-parser');
+
+// Require Models
+require('./models/User'); // USER MODEL
+
+// Set Routes
+const routes = require('./routes');
+
+// Connect MongoDB with mLab.com
+mongoose.connect(keys.mongoURI);
+
+// Declare app
 const app = express();
 
-const routes = require('./routes');
+// Setup Body Parser
+app.use(bodyParser.json());
 
 app.use('/static', express.static('public'));
 
-// Set view engine with Pug
-app.set('view engine', 'pug');
-
-// Set up routes
-// app.use(routes);
-
-// app.use(function (req, res, next) {
-//   const err = new Error('Page not found');
-//   err.status = 404;
-//   next(err);
-// })
-
-// Listen on port 3000
-app.listen(process.env.PORT || 5000, function(){
+// Listen on port 5000 in Development
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
