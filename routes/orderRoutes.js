7ed3cf8f576f93e5a2fc4bc75667630385a197f2
Michael McDevitt <mmcdevi1@gmail.com>
2018-04-12 21:09:23 -0400

    error handling for order form

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const passportService = require('../services/passport');
 const passport = require('passport');
+const Orders = require('../controllers/ordersController');
 
 // Require Order Model
 const Order = require('../models/Order');
@@ -12,28 +13,12 @@ const requireLogin     = passport.authenticate('local', { session: false });
 const authenticateUser = passport.authenticate('jwt', { session: false }); // T
 
 // Order create POST route
-router.post('/api/order/new', authenticateUser, (req, res, next) => {
-  const order = {
-    _user: req.user.id,
-    address: {
-      street: '50 e sunnsydie lane',
-      city: 'irvington',
-      state: 'ny',
-      zipcode: '10533'
-    }
-  }
+router.post('/api/order/new', authenticateUser, Orders.new);
 
-  Order.create(order, (err, order) => {
-    if (err) { return next(err) }
-    res.send( {order: order, user: order._user} )
-  })
-})
-
-router.get('/a/orders', authenticateUser, (req, res) => {
-  // Order.find({ _user: req.user.id }, (err, order) => {
-  //   res.send(order)
-  // })
-  Order.findOne({ address: {city: 'irvington'} })
+router.get('/api/orders', authenticateUser, (req, res) => {
+  Order.find({ _user: req.user.id }, (err, order) => {
+    res.send(order)
+  });
 })
 
 module.exports = router;
