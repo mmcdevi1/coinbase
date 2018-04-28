749edaf619fda1d3adae7c07490833d2ffd8d128
Michael McDevitt <mmcdevi1@gmail.com>
2018-04-28 14:01:16 -0400

    fix heroku issues

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(authRoutes);
 
 
 // Listen on port 5000 in Development
-const PORT = process.env.PORT || 5000;
-app.listen(process.env.PORT || 5000, () => {
-  console.log(`Listening on port ${PORT}`);
+// const PORT = process.env.PORT || 5000;
+app.listen(process.env.PORT || 5000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
