5c1c6a9db2889a95dba484c2785128042685469f
mmcdevi1 <mmcdevi1@gmail.com>
2018-05-14 20:33:45 -0400

    cart item

--- controllers/cartItemsController.js
+++ controllers/cartItemsController.js
@@ -1,3 +1,11 @@
 const CartItem = require('../models/CartItem');
 const Cart = require('../models/Cart');
 const User = require('../models/User');
+
+exports.new = (req, res, next) => {
+	CartItem
+		.create({
+			userId: req.user.id,
+			cartId: req.params.id,
+		})
+}
