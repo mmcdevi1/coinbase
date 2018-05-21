a4ef36c67f62794c5a0916d12cc535ac48ae3f07
GiarcEnoredlac <calderonecraig@gmail.com>
2018-05-21 19:22:56 -0400

    add ract native ios android packages

---
+++ ios/Pods/Target Support Files/Pods-dna_blockchain/Pods-dna_blockchain-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_dna_blockchain : NSObject
+@end
+@implementation PodsDummy_Pods_dna_blockchain
+@end
---
+++ ios/Pods/Target Support Files/Pods-dna_blockchainTests/Pods-dna_blockchainTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_dna_blockchainTests : NSObject
+@end
+@implementation PodsDummy_Pods_dna_blockchainTests
+@end
